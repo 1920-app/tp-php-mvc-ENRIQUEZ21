33c83e1465a33832d094045220eaa6d4392cdcf6
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2020-04-28 22:59:09 +0200

    Move part of the content of index.php in the files list.php and edit.php

--- views/edit.php
+++ views/edit.php
@@ -1,1 +1,27 @@
 <?php
+
+
+$nom = "";
+$prenom = "";
+$id = "";
+if($_GET["action"]=="modifier") {
+    $reponse = $db->prepare("select * from users where id=".$_GET["id"]);
+    $reponse->execute();
+
+    while($user = $reponse->fetch()){
+        $nom = $user["nom"];
+        $prenom = $user["prenom"];
+        $id = $user["id"];
+    }
+}
+
+
+?>
+
+<form action="index.php" method="get">
+    Nom : <input type="text" name="nom" value="<?php echo $nom; ?>"/>
+    Prenom : <input type="text" name="prenom" value="<?php echo $prenom; ?>"/>
+    <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+    <input type="hidden" name="action" value="save"/>
+    <input type="submit"/>
+</form>
